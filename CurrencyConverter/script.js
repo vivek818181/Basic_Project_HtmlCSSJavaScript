920683f15d9516a0aac0e92bc9e70f707da86cd2
Vivek <vivekgupta2k06@gamil.com>
2026-01-24 15:18:13 +0530

    Updating Currency Convertar

--- CurrencyConverter/script.js
+++ CurrencyConverter/script.js
@@ -1,15 +1,34 @@
-const currencyFirstE1=document.getElementById("Currency-first");
-const worthFirstE1E1=document.getElementById("Currency-first");
-const currencySecondE1=document.getElementById("Currency-second");
-const worthSecondE1=document.getElementById("Currency-second");
+const currencyFirstEl = document.getElementById("Currency-first");
+const worthFirstEl = document.getElementById("worth-first");
 
-const ExchangeRateE1 =document.getElementById("exchange-rate");
+const currencySecondEl = document.getElementById("Currency-second");
+const worthSecondEl = document.getElementById("worth-second");
 
+const exchangeRateEl = document.getElementById("exchange-rate");
 
-function updateRate(){
-    console.log("called")
+function updateRate() {
+    fetch(`https://v6.exchangerate-api.com/v6/80c8c5f864a010d1581c8bac/latest/${currencyFirstEl.value}`)
+        .then(res => res.json())
+        .then(data => {
+            const rate = data.conversion_rates[currencySecondEl.value];
+
+            exchangeRateEl.innerText =
+                `1 ${currencyFirstEl.value} = ${rate} ${currencySecondEl.value}`;
+
+            worthSecondEl.value = (worthFirstEl.value * rate).toFixed(2);
+        })
+        .catch(err => {
+            exchangeRateEl.innerText = "Error fetching exchange rate";
+            console.error(err);
+        });
 }
-    currencyFirstE1.addEventListener("change",updateRate)
-    currencySecondE1.addEventListener("change",updateRate)
-    worthFirstE1E1.addEventListener("input",updateRate)
+
+currencyFirstEl.addEventListener("change", updateRate);
+currencySecondEl.addEventListener("change", updateRate);
+worthFirstEl.addEventListener("input", updateRate);
+
+// Auto run on load
+updateRate();
+
+
 
