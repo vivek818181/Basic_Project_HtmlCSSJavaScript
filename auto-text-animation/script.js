842c210cad2d6e5893f7c97de3d87a0afe512093
Vivek <vivekgupta2k06@gamil.com>
2026-01-27 19:55:49 +0530

    JavaScript modify

--- auto-text-animation/script.js
+++ auto-text-animation/script.js
@@ -1,21 +1,25 @@
-const containerE1 =document.querySelector(".container")
+const containerE1 = document.querySelector(".container");
 
-const careers = ["Student","Wed Devloper","Data Scientis"];
+const careers = ["Student", "Wed Devloper", "Data Scientis"];
 
-let index =0
+let index = 0;
 let charIndex = 0;
-updateText()
-function updateText(){
+
+updateText();
+
+function updateText() {
     charIndex++;
-containerE1.innerHTML = `<h1>I am a ${careers[index].slice(0,charIndex)}</h1>`;
 
-if(charIndex ==careers[index].length){
-    index++;
-    index = 0;
-}
-if(index == careers.length){
-    index = 0;
-}
+    containerE1.innerHTML = `<h1>I am a ${careers[index].slice(0, charIndex)}</h1>`;
 
-setTimeout(updateText,400);
+    if (charIndex === careers[index].length) {
+        index++;
+        charIndex = 0;   
+    }
+
+    if (index === careers.length) {
+        index = 0;
+    }
+
+    setTimeout(updateText, 400);
 }
